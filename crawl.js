abff556e5be25eb8be6602cb80f87b54852e2b2e
Emil Malmsten <emil_malmsten@hotmail.com>
2023-04-11 11:36:04 +0700

    Add main function and crawlPage function

--- crawl.js
+++ crawl.js
@@ -1,7 +1,5 @@
 const { JSDOM } = require('jsdom')
 
-const htmlBody = '<html><body><a href="/about"><span>Go to Boot.dev</span></a></body></html>'
-
 const getURLsFromHTML = (htmlBody, baseURL) => {
     const dom = new JSDOM(htmlBody);
     const links = dom.window.document.querySelectorAll('a[href]');
@@ -14,7 +12,6 @@ const getURLsFromHTML = (htmlBody, baseURL) => {
             return  urlObj.href;
         }
     })
-
     return absoluteUrls
 }
 
@@ -30,9 +27,48 @@ const normalizeURL = (url) => {
     }
 }
 
-getURLsFromHTML(htmlBody, 'https://wagslane.dev')
+const crawlPage = async (baseURL, currentURL, pages) => {
+    if (!currentURL.includes(baseURL)) {
+        console.log(`${currentURL} not on ${baseURL}`)
+        return pages;
+    }
+    
+    const normCurrURL = await normalizeURL(currentURL);
+    if (pages[normCurrURL]) {
+        pages[normCurrURL] += 1;
+        console.log(`already visited ${normCurrURL}, returning...`)
+        return pages;
+    }
+    pages[normCurrURL] = 1;
+
+    try {
+        console.log(`Requesting ${normCurrURL}`);
+        const response = await fetch(`https://${normCurrURL}`);
+
+        if (!response.ok) {
+            console.error(`Error: HTTP status code ${response.status}`);
+            return pages;
+        }
+        const contentType = response.headers.get('content-type');
+        if (!contentType || !contentType.includes('text/html')) {
+            console.error('Error: Response is not HTML');
+            return pages;
+        }
+        const html = await response.text();
+        const urls = getURLsFromHTML(html, baseURL)
+        for (let url of urls) {
+            pages = await crawlPage(baseURL, url, pages);
+        }
+        return pages
+
+    } catch (error) {
+        console.error(`Error: ${error}`);
+        return pages;
+    }
+}
 
 module.exports = {
     getURLsFromHTML,
     normalizeURL,
+    crawlPage
 }
